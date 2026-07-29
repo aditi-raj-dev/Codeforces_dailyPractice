a396a8c57c9daecbb75262c35e5c558226299e3f
aditi-raj-dev <aditi.raj.tech@gmail.com>
2026-07-29 14:50:02 +0530

    DAY 16

---
+++ hq.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == 'H' || ch == 'Q' || ch == '9') {
+                System.out.println("YES");
+                sc.close();
+                return;
+            }
+        }
+
+        System.out.println("NO");
+
+        sc.close();
+    }
+}
