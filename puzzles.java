9e597f7dce71a3963db7f72ce495aa3f3b27bfdb
aditi-raj-dev <aditi.raj.tech@gmail.com>
2026-08-16 18:25:28 +0530

    DAY 24

---
+++ puzzles.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class puzzles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] puzzles = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            puzzles[i] = sc.nextInt();
+        }
+
+        Arrays.sort(puzzles);
+
+        int answer = Integer.MAX_VALUE;
+
+        for (int i = 0; i <= m - n; i++) {
+            int difference = puzzles[i + n - 1] - puzzles[i];
+
+            answer = Math.min(answer, difference);
+        }
+
+        System.out.println(answer);
+
+        sc.close();
+    }
+}
