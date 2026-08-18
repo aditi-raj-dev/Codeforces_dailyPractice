2ecc0a7ec7b0e9a4feb05da6f9d8e0e34f0f7885
aditi-raj-dev <aditi.raj.tech@gmail.com>
2026-08-18 19:53:13 +0530

    DAY 26

---
+++ drinks.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class drinks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        double sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            double percentage = sc.nextDouble();
+            sum += percentage;
+        }
+
+        double answer = sum / n;
+
+        System.out.println(answer);
+
+        sc.close();
+    }
+}
